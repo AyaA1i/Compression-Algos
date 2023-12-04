cc10962bb92596cd6ad45950218cd69caa487c9a
Maya Zain El-Din <102183224+MayaZayn@users.noreply.github.com>
2023-12-04 12:31:58 +0200

    Use Gui

--- Vector Quantization/Main.java
+++ Vector Quantization/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        //GUI.createAndShowGUI();
-        VectorQuantization vq = new VectorQuantization();
-        vq.compress("inputC.txt", "outputC.txt");
-        vq.decompress("inputC.txt", "outputC.txt");
+        GUI.createAndShowGUI();
+        //VectorQuantization vq = new VectorQuantization();
+        //vq.compress("inputC.txt", "outputC.txt");
+        //vq.decompress("inputC.txt", "outputC.txt");
     }
 }
